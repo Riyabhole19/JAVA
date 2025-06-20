0d5646c4de3badf6dd86c3645c2c9cb53b521dcd
Riyabhole19 <riyabhole19@gmail.com>
2025-06-20 21:15:24 +0530

    Add files via upload

---
+++ Pat2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Pat2
+{
+	public static void main(String [] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the number :- ");
+		int n=sc.nextInt();
+		Pattern (n);
+	}
+	static void Pattern(int n)
+	{	
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=0;j<n-i;j++)
+			{
+				System.out.print(" ");
+			}
+		for(int j=0;j<n;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+}
